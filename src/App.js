e6d21c0e6f4b2e720f2502925bf62bf2fd612830
Bogdan Negura <id.bogdann@yahoo.com>
2021-01-20 22:34:08 +0000

    log out functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { Feed } from './app/components/feed/feed.component';
 import { Header } from './app/components/header';
 import { Login } from './app/components/login/login.component';
 import { Sidebar } from './app/components/sidebar/sidebar.component';
-import { selectUser } from './features/userSlice';
+import { auth } from './app/firebase';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
 
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoUrl
+        }))
+      } else {
+        // user is logged out
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
--- src/app/components/header/header.component.jsx
+++ src/app/components/header/header.component.jsx
@@ -7,8 +7,18 @@ import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../features/userSlice';
+import { auth } from '../../firebase';
 
 const Header = () => {
+
+    const dispatch = useDispatch()
+    const logoutOfApp = () => {
+        dispatch(logout())
+        auth.signOut()
+    }
+
     return (
         <div className="header">
             <div className="header__left">
@@ -24,7 +34,7 @@ const Header = () => {
                 <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
                 <HeaderOption Icon={ChatIcon} title="Messaging"/>
                 <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
-                <HeaderOption avatar="https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg" title="me"/>
+                <HeaderOption onClick={logoutOfApp} avatar="https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg" title="me"/>
             </div>
         </div>
     )
